529e9612416e7bd7fa8dc8673ae37f3dd8b695b5
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 19:23:09 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan12-Aritmatika/src/pboif2/pkg10119083/latihan12/aritmatika/PBOIF210119083Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * Nama      : Rafsan Zen Mustaofa
+ * Kelas     : IF2
+ * NIM       : 10119083
+ * Deskripsi : aritmatika
+ */
+public class PBOIF210119083Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 100;
+        int b = 20;
+        int c = 25;
+        int d = 50;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
